e28f847599d14f6badfc5ffcf19e1d58ed5db671
Jung <rickrl0315db@gmail.com>
2021-04-19 16:01:04 +0900

    html for 문

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template
+from data import Articles
 
 
 app = Flask(__name__)
@@ -19,7 +20,9 @@ def about():
 
 @app.route('/articles')
 def articles():
-    return render_template("articles.html", hello="Gary Kim")
-    
+    articles = Articles()
+    #print(articles[0]['title'])
+    return render_template("articles.html", articles=articles)
+
 if __name__ == "__main__": # 처음 서버 띄울때
     app.run() # http://localhost:5000/ default
